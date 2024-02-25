7fa6978adfbbd858dd3a000e5de854220a4f781d
yaredybk <yb12ybk@gmail.com>
2024-02-24 19:38:39 -0800

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,14 +4,12 @@
  * swap_array - swaps values of array at index ind and ind + 1
  * @array: main array to sort
  * @ind: index number to check
- * @order: 1 for ascending -1 for descending
  *
  * Return: true if swaped, else false
  */
-bool swap_array(int *array, int ind, int order)
+bool swap_array(int *array, int ind)
 {
-	if ((order && (array[ind] > array[ind + 1])) ||
-			(order < 0 && (array[ind] < array[ind + 1])))
+	if (array[ind + 1] && (array[ind] > array[ind + 1]))
 	{
 		int n = array[ind];
 
@@ -44,10 +42,10 @@ void bubble_sort(int *array, size_t size)
 		swaped = false;
 		for (i = 0; i < n - 1; i++)
 		{
-			swaped |= swap_array(array, i, 1);
+			swaped |= swap_array(array, i);
 			if (swaped)
 				print_array(array, size);
-			nn = i;
+			nn = i + 1;
 		}
 		n = nn;
 	} while (swaped);
