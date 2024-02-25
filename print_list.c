e20c762684d9ef9de10b07428fef979f49f27c4b
yaredybk <yb12ybk@gmail.com>
2024-02-24 20:39:47 -0800

    fix

--- print_list.c
+++ print_list.c
@@ -8,18 +8,33 @@
  */
 void print_list(const listint_t *list)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-	printf("%d", list->n);
-        ++i;
-        list = list->next;
-	if (i > 10)
-		break;
-    }
-    printf("\n");
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		if (list->next == NULL)
+			break;
+		list = list->next;
+		if (i > 10)
+			break;
+	}
+	printf("\n");
+
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->prev;
+		if (i > 10)
+			break;
+	}
+	printf("\n");
 }
